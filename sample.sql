4fdb871904568728cdbb8e37ce65bd778e57367e
William Grzybowski <william@agencialivre.com.br>
2009-04-12 11:01:01 -0300

    sample sql table structure

---
+++ sample.sql
@@ -0,0 +1,17 @@
+-- This is a sample for a table storing unix users
+-- pam-pgsql
+
+-- William Grzybowski <william@agencialivre.com.br>
+
+begin;
+
+	CREATE TABLE `account` (
+
+		username varchar(255) UNIQUE NOT NULL,
+		password varchar(200),
+		expired timestamptz,
+		newtok varchar(200),
+
+	);
+
+end;
