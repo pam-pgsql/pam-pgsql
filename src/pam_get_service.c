d8a796fce0f809949dc0cf2f01ec8b2a1cfa005e
William Grzybowski <william@agencialivre.com.br>
2009-04-15 21:15:08 -0300

    including stdio.h for service, it could cause some warnings

--- src/pam_get_service.c
+++ src/pam_get_service.c
@@ -1,16 +1,23 @@
 /*
- * Copyright (c) 2000. Leon Breedt, Copyright (c) 2002 David D.W. Downey
+ * Copyright (c) 2000. Leon Breedt, 
+ *               2002 David D.W. Downey
+ *               2009 William Grzybowski <william@agencialivre.com.br>
+ *
  */
 
-/* $Id: pam_get_service.c,v 1.2 2000/06/25 10:01:41 ljb Exp $ */
-#include <security/pam_modules.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <security/pam_modules.h>
 
-const char *pam_get_service(pam_handle_t *pamh)
+const char *
+pam_get_service(pam_handle_t *pamh)
 {
-    const char *service = NULL;
+
+	const char *service = NULL;
 
 	if(pam_get_item(pamh, PAM_SERVICE, (void *) &service) != PAM_SUCCESS)
-        return NULL;
-    return service;
+		return NULL;
+
+	return service;
+
 }
